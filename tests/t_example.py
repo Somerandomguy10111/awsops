021b69d544b950156940ef3bc7ec0740b19e3858
Dan Hol <daniel.hollarek@googlemail.com>
2024-04-21 06:44:14 +0200

    Initial commit

---
+++ tests/t_example.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+# noinspection PyUnresolvedReferences
+class ExampleTest(TestCase):
+    def test_print_hi(self):
+        print("Hi, this test was sucessful!")
+        self.assertTrue(True)
