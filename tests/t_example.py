dd8183bb6d46b3790f07a09c09a33684aeb887e3
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-02-17 22:01:09 +0100

    General: Deleted empty tests, github actions

--- tests/t_example.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-
-# noinspection PyUnresolvedReferences
-class ExampleTest(TestCase):
-    def test_print_hi(self):
-        print("Hi, this test was sucessful!")
-        self.assertTrue(True)
